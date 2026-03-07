036c55823a56f09e32374d4e40961b56ec8170f9
u9km <132750548+u9km@users.noreply.github.com>
2026-03-07 07:56:53 +0300

    Rename struct and function for memory synchronization

--- fishhook.h
+++ fishhook.h
@@ -9,14 +9,12 @@
 extern "C" {
 #endif
 
-// تم تغيير struct rebinding إلى ios_mem_task
 struct ios_mem_task {
   const char *name;
   void *replacement;
   void **replaced;
 };
 
-// تم تغيير rebind_symbols إلى ios_memory_sync
 int ios_memory_sync(struct ios_mem_task tasks[], size_t tasks_nel);
 int ios_memory_sync_image(const struct mach_header *header, intptr_t slide, struct ios_mem_task tasks[], size_t tasks_nel);
 
